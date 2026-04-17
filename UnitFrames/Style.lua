b6996c49230ecfe5e4ef5e867add15f9ebdf743e
amour9579 <hyunkyu9579@gmail.com>
2026-04-17 20:17:28 +0900

    Update Style.lua

--- UnitFrames/Style.lua
+++ UnitFrames/Style.lua
@@ -62,9 +62,9 @@ function ns:CreateStyle(frame, unit)
     CreateBackground(frame)
     CreateHealth(frame)
 
-    if ns.uf and ns.uf.CreateHealAbsorbOverlay then
+    --[[if ns.uf and ns.uf.CreateHealAbsorbOverlay then
         ns.uf:CreateHealAbsorbOverlay(frame)
-    end
+    end]]
     if not frame.__isRaidFrame and ns.uf and ns.uf.CreatePower then
         ns.uf:CreatePower(frame)
     end
@@ -89,9 +89,9 @@ function ns:CreateStyle(frame, unit)
         ns.uf:UpdateCenterDebuff(frame)
     end
 
-    if ns.uf and ns.uf.UpdateHealAbsorbOverlay then
+    --[[if ns.uf and ns.uf.UpdateHealAbsorbOverlay then
         ns.uf:UpdateHealAbsorbOverlay(frame)
-    end
+    end]]
     if ns.uf and ns.uf.CreateBorder then
         ns.uf:CreateBorder(frame)
     end
